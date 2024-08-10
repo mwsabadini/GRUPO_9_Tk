ab4baf80fbf50869b9d674ccf7fbf8fccd0a3690
mauricio5225 <mauroidolo5225@gmail.com>
2024-08-10 16:40:44 -0300

    imagen fondo

--- grupo_9-tk/grupo_9-tk.py
+++ grupo_9-tk/grupo_9-tk.py
@@ -8,6 +8,7 @@ simple que muestra la hora actual en la esquina superior derecha."""
 import tkinter as tk
 from tkinter import Menu
 import time
+from PIL import Image, ImageTk
 
 # Función para actualizar el reloj
 def actualizar_reloj():
@@ -31,16 +32,24 @@ def salir():
 # ventana principal
 root = tk.Tk()
 root.title("Página de Inicio Interactiva del Grupo 9")
-root.geometry("800x500")
-root.configure(bg="lightgreen")  # Color verde claro para la ventana principal
+root.geometry("500x300")
+root.configure(bg="yellow")  # Color verde claro para la ventana principal
+
+imagen_fondo = Image.open("D:/TKINDER-INFO2024/grupo9/grupo_9-tk/imagen_info.png")
+imagen_fondo = imagen_fondo.resize((500, 300), Image.Resampling.LANCZOS)
+imagen_fondo = ImageTk.PhotoImage(imagen_fondo)
+
+label_fondo = tk.Label(root, image=imagen_fondo)
+label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+
 
 # armado del menú desplegable
-menu_bar = Menu(root, bg="lightgreen", fg="brown")
+menu_bar = Menu(root, bg="red", fg="brown")#no funciona color
 root.config(menu=menu_bar)
 
-menu_navegacion = Menu(menu_bar, tearoff=0, bg="yellow", fg="black",
+menu_navegacion = Menu(menu_bar, tearoff=0, bg="lightblue", fg="black",
                        activebackground="white", activeforeground="black")
-menu_bar.add_cascade(label="Navegar", menu=menu_navegacion, background="lightblue")  # Fondo celeste para "Navegar"
+menu_bar.add_cascade(label="Navegar", menu=menu_navegacion, background="orange")  # Fondo celeste para "Navegar"
 menu_navegacion.add_command(label="Sección 1", command=mostrar_seccion1)
 menu_navegacion.add_command(label="Sección 2", command=mostrar_seccion2)
 menu_navegacion.add_command(label="Sección 3", command=mostrar_seccion3)
@@ -48,12 +57,26 @@ menu_navegacion.add_separator()
 menu_navegacion.add_command(label="Salir", command=salir)   
 
 # Etiqueta principal
-etiqueta_principal = tk.Label(root, text="Bienvenido a la Página de Inicio del Grupo 9", font=('Arial', 25))
-etiqueta_principal.pack(pady=100)
+etiqueta_principal = tk.Label(root, text="INFORMATORIO 2024 - COMISION 4",
+                              font=('Arial', 25), fg="black", bg="lightgrey")
+etiqueta_principal.pack(pady=30)
+etiqueta_principal = tk.Label(root, text="Bienvenido a la Página de Inicio del Grupo 9",
+                              font=('Arial', 25), fg="black", bg="lightgrey")
+etiqueta_principal.pack(pady=10)
 
+# Crear Listbox y agregarlo a la esquina inferior derecha
+listbox = tk.Listbox(root, height=9, width=20)
+listbox.place(relx=1.0, rely=1.0, anchor='se', x=-20, y=-10)
 
+# Crear título "Grupo 9" y posicionarlo un poco más a la izquierda y abajo
+titulo = tk.Label(root, text="Grupo 9", font=('arial', 14))
+titulo.place(relx=1.0, rely=1.0, anchor='se', x=-60, y=-180)
 
-root.mainloop()
+# Lista de nombres de los integrantes
+integrantes = ["Noemi Ciscka", "Burgos Mauricio", "Esteban Ayala", "Yamila"]
 
-
-
+# Agregar los nombres de los integrantes a la lista
+for integrante in integrantes:
+    listbox.insert(tk.END, integrante)
+    
+root.mainloop()    
